6bc93dd4946f6c7b59a8f8a099e85db43cd59dc3
Saul Vega <saul.vg1@gmail.com>
2021-01-07 13:09:51 -0700

    finish Basics: working with static files

--- nextjscourse/pages/about.js
+++ nextjscourse/pages/about.js
@@ -11,6 +11,7 @@ export default function About() {
       </Head>
       <h1>About</h1>
       <p>This is the about page </p>
+      <img src="/image/goku.png" />
       <style global jsx>
         {`
           body {
