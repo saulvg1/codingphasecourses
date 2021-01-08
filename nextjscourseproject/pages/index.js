4915ac2baf9fb1fde82466562e9a621b3035fea8
Saul Vega <saul.vg1@gmail.com>
2021-01-07 19:35:15 -0700

    finish section Project: create templates and layouts

--- nextjscourseproject/pages/index.js
+++ nextjscourseproject/pages/index.js
@@ -1,52 +1,13 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Layout from '../components/layout.js';
 
 export default function Home() {
   return (
-    <div id="wrapper" className={styles.container}>
+    <Layout id="wrapper" className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Saul's Blog</title>
       </Head>
-
-      {/* Header */}
-      <header id="header" className="alt">
-        <a href="index.html" className="logo">
-          <strong>Forty</strong> <span>by HTML5 UP</span>
-        </a>
-        <nav>
-          <a href="#menu">Menu</a>
-        </nav>
-      </header>
-      {/* Menu */}
-      <nav id="menu">
-        <ul className="links">
-          <li>
-            <a href="index.html">Home</a>
-          </li>
-          <li>
-            <a href="landing.html">Landing</a>
-          </li>
-          <li>
-            <a href="generic.html">Generic</a>
-          </li>
-          <li>
-            <a href="elements.html">Elements</a>
-          </li>
-        </ul>
-        <ul className="actions stacked">
-          <li>
-            <a href="#" className="button primary fit">
-              Get Started
-            </a>
-          </li>
-          <li>
-            <a href="#" className="button fit">
-              Log In
-            </a>
-          </li>
-        </ul>
-      </nav>
       {/* Banner */}
       <section id="banner" className="major">
         <div className="inner">
@@ -75,7 +36,7 @@ export default function Home() {
         <section id="one" className="tiles">
           <article>
             <span className="image">
-              <img src="images/pic01.jpg" alt="" />
+              <img src="/assets/images/pic01.jpg" alt="" />
             </span>
             <header className="major">
               <h3>
@@ -88,7 +49,7 @@ export default function Home() {
           </article>
           <article>
             <span className="image">
-              <img src="images/pic02.jpg" alt="" />
+              <img src="/assets/images/pic02.jpg" alt="" />
             </span>
             <header className="major">
               <h3>
@@ -101,7 +62,7 @@ export default function Home() {
           </article>
           <article>
             <span className="image">
-              <img src="images/pic03.jpg" alt="" />
+              <img src="/assets/images/pic03.jpg" alt="" />
             </span>
             <header className="major">
               <h3>
@@ -114,7 +75,7 @@ export default function Home() {
           </article>
           <article>
             <span className="image">
-              <img src="images/pic04.jpg" alt="" />
+              <img src="/assets/images/pic04.jpg" alt="" />
             </span>
             <header className="major">
               <h3>
@@ -127,7 +88,7 @@ export default function Home() {
           </article>
           <article>
             <span className="image">
-              <img src="images/pic05.jpg" alt="" />
+              <img src="/assets/images/pic05.jpg" alt="" />
             </span>
             <header className="major">
               <h3>
@@ -140,7 +101,7 @@ export default function Home() {
           </article>
           <article>
             <span className="image">
-              <img src="images/pic06.jpg" alt="" />
+              <img src="/assets/images/pic06.jpg" alt="" />
             </span>
             <header className="major">
               <h3>
@@ -177,113 +138,6 @@ export default function Home() {
           </div>
         </section>
       </div>
-      {/* Contact */}
-      <section id="contact">
-        <div className="inner">
-          <section>
-            <form method="post" action="#">
-              <div className="fields">
-                <div className="field half">
-                  <label htmlFor="name">Name</label>
-                  <input type="text" name="name" id="name" />
-                </div>
-                <div className="field half">
-                  <label htmlFor="email">Email</label>
-                  <input type="text" name="email" id="email" />
-                </div>
-                <div className="field">
-                  <label htmlFor="message">Message</label>
-                  <textarea
-                    name="message"
-                    id="message"
-                    rows={6}
-                    defaultValue={''}
-                  />
-                </div>
-              </div>
-              <ul className="actions">
-                <li>
-                  <input
-                    type="submit"
-                    defaultValue="Send Message"
-                    className="primary"
-                  />
-                </li>
-                <li>
-                  <input type="reset" defaultValue="Clear" />
-                </li>
-              </ul>
-            </form>
-          </section>
-          <section className="split">
-            <section>
-              <div className="contact-method">
-                <span className="icon solid alt fa-envelope" />
-                <h3>Email</h3>
-                <a href="#">information@untitled.tld</a>
-              </div>
-            </section>
-            <section>
-              <div className="contact-method">
-                <span className="icon solid alt fa-phone" />
-                <h3>Phone</h3>
-                <span>(000) 000-0000 x12387</span>
-              </div>
-            </section>
-            <section>
-              <div className="contact-method">
-                <span className="icon solid alt fa-home" />
-                <h3>Address</h3>
-                <span>
-                  1234 Somewhere Road #5432
-                  <br />
-                  Nashville, TN 00000
-                  <br />
-                  United States of America
-                </span>
-              </div>
-            </section>
-          </section>
-        </div>
-      </section>
-      {/* Footer */}
-      <footer id="footer">
-        <div className="inner">
-          <ul className="icons">
-            <li>
-              <a href="#" className="icon brands alt fa-twitter">
-                <span className="label">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon brands alt fa-facebook-f">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon brands alt fa-instagram">
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon brands alt fa-github">
-                <span className="label">GitHub</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon brands alt fa-linkedin-in">
-                <span className="label">LinkedIn</span>
-              </a>
-            </li>
-          </ul>
-          <ul className="copyright">
-            <li>© Untitled</li>
-            <li>
-              Design: <a href="https://html5up.net">HTML5 UP</a>
-            </li>
-          </ul>
-        </div>
-      </footer>
-    </div>
+    </Layout>
   );
 }
